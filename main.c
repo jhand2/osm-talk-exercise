338df535f706e2f204a1a3d9ec13c03769ae63bf
Jordan Hand <jorhand@microsoft.com>
2020-02-11 14:32:39 -0800

    Add cmdline arg

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char **argv)
 {
-    printf("I <3 Open Source!\n");
+    if (argc < 2)
+        printf("I <3 Open Source!\n");
+    else
+        printf("%s <3's Open Source!\n")
+
     return 0;
 }
