96de05a5f47ae5f39025f532a897e3a698e7edb9
Jordan Hand <jorhand@microsoft.com>
2020-02-11 14:35:29 -0800

    Add semicolon

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
     if (argc < 2)
         printf("I <3 Open Source!\n");
     else
-        printf("%s <3's Open Source!\n")
+        printf("%s <3's Open Source!\n");
 
     return 0;
 }
